be0b5d1ef0f242337b75ab89a040a795a7cec3a3
Tyler Turner <tylerturner@Tylers-Mac-mini.local>
2019-07-21 18:59:39 -0700

    make file update and start unit test files

---
+++ projects/turnerty/dominion/unittest2.c
@@ -0,0 +1,63 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+int main() {
+
+}
+
+int minion(int handPos, int choice1, int choice2, struct gameState *state)
+{
+  int currentPlayer = whoseTurn(state);
+  int i;
+  int j;
+  state->numActions++;
+  //discard card from hand
+  discardCard(handPos, currentPlayer, state, 0);
+
+  if (choice1)		//+2 coins
+    {
+    state->coins = state->coins + 2;
+    }
+
+  else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
+  {
+    //discard hand
+    while(numHandCards(state) > 0)
+    {
+      discardCard(handPos, currentPlayer, state, 0);
+    }
+
+    //draw 4
+    for (i = 0; i < 4; i++)
+      {
+        drawCard(currentPlayer, state);
+      }
+
+    //other players discard hand and redraw if hand size > 4
+    for (i = 0; i < state->numPlayers; i++)
+      {
+        if (i != currentPlayer)
+        {
+          if ( state->handCount[i] > 4 )
+            {
+              //discard hand
+              while( state->handCount[i] > 0 )
+              {
+                discardCard(handPos, i, state, 0);
+              }
+
+              //draw 4
+              for (j = 0; j < 4; j++)
+              {
+                drawCard(i, state);
+              }
+            }
+        }
+      }
+    }
+  return 0;
+}
